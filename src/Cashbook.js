f780d4212e06adb6cac013615c8220909fc5def6
frank.park <frank.park@kakaocorp.com>
2017-11-25 01:48:27 +0900

    remove assets & codes we don't need

---
+++ src/Cashbook.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Cashbook.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="Cashbook">
+        <header className="Cashbook-header">
+          <h1 className="Cashbook-title">Cashbook</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;

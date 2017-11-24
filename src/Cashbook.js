15156446c376bb8872e816223f0df605297a4dd6
frank.park <frank.park@kakaocorp.com>
2017-11-25 02:01:16 +0900

    add sass css pre-processor and some styles

--- src/Cashbook.js
+++ src/Cashbook.js
@@ -1,16 +1,17 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './Cashbook.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="Cashbook">
-        <header className="Cashbook-header">
-          <h1 className="Cashbook-title">Cashbook</h1>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="Cashbook">
+                <header className="Cashbook-header">
+                    <h1 className="Cashbook-title">Cashbook</h1>
+                    yet another cashbook
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;

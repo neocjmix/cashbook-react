6adc02495afaebe076ef24cbb643f368b55827ab
frank.park <frank.park@kakaocorp.com>
2017-11-25 02:19:51 +0900

    basic array sample data & jsx

--- src/Cashbook.js
+++ src/Cashbook.js
@@ -2,6 +2,13 @@ import React, {Component} from 'react';
 import './Cashbook.css';
 
 class App extends Component {
+    constructor(){
+        super();
+        this.state = {
+            records : ["foo","bar"]
+        }
+    }
+
     render() {
         return (
             <div className="Cashbook">
@@ -9,6 +16,9 @@ class App extends Component {
                     <h1 className="Cashbook-title">Cashbook</h1>
                     yet another cashbook
                 </header>
+                <ul className="Cashbook-records">
+                    {this.state.records.map(record => <li>{record}</li>)}
+                </ul>
             </div>
         );
     }

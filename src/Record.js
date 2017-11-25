a7a187012511e3cacf2b34676de089c32c90f7a0
frank.park <frank.park@kakaocorp.com>
2017-11-26 00:10:43 +0900

    refactoring

--- src/Record.js
+++ src/Record.js
@@ -3,28 +3,30 @@ import './Record.css';
 
 class Record extends Component {
     render() {
+        const data = this.props.data;
+
         return <div className="Record">
-            <label className="record-inout" htmlFor={"record-out-" + this.props.data.id}>
-                <input type="radio" name={"record-" + this.props.data.id + "-inout"}
-                       id={"record-out-" + this.props.data.id} autoFocus
-                       checked={!this.props.data.isIncome}
+            <label className="record-inout" htmlFor={"record-out-" + data.id}>
+                <input type="radio" name={"record-" + data.id + "-inout"}
+                       id={"record-out-" + data.id} autoFocus
+                       checked={!data.isIncome}
                        onChange={e => this.props.updateRecord({isIncome: !e.target.checked})}/>
                 지출
             </label>
-            <label className="record-inout" htmlFor={"record-in-" + this.props.data.id}>
-                <input type="radio" name={"record-" + this.props.data.id + "-inout"}
-                       id={"record-in-" + this.props.data.id}
-                       checked={this.props.data.isIncome}
+            <label className="record-inout" htmlFor={"record-in-" + data.id}>
+                <input type="radio" name={"record-" + data.id + "-inout"}
+                       id={"record-in-" + data.id}
+                       checked={data.isIncome}
                        onChange={e => this.props.updateRecord({isIncome: e.target.checked})}/>
                 수입
             </label>
-            <input type="text" className="record-title" name={"record-" + this.props.data.id + "-title"}
-                   value={this.props.data.title} placeholder="title"
+            <input type="text" className="record-title" name={"record-" + data.id + "-title"}
+                   value={data.title} placeholder="title"
                    onChange={e => this.props.updateRecord({title : e.target.value})} />
-            <input type="number" className="record-amount" name={"record-" + this.props.data.id + "-amount"}
-                   value={this.props.data.amount}
+            <input type="number" className="record-amount" name={"record-" + data.id + "-amount"}
+                   value={data.amount}
                    onChange={e => this.props.updateRecord({amount: e.target.value})} min="0"/>
-            <button aria-label="delete" className="delete" onClick={() => this.props.deleteRecord(this.props.data)}>-
+            <button aria-label="delete" className="delete" onClick={() => this.props.deleteRecord(data)}>-
             </button>
         </div>
     }
